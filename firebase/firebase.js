ca3fb10ffa5c4fc529730051b569ce54318c10df
Dmytro Lysenko <crystaljoint@gmail.com>
2021-05-30 19:37:33 +0300

    Adding Contact Us section and send form to firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCnnE873cG7fEsNlbWZek0KRT5WN7YQCtc",
+    authDomain: "responsive-website-1c15e.firebaseapp.com",
+    projectId: "responsive-website-1c15e",
+    storageBucket: "responsive-website-1c15e.appspot.com",
+    messagingSenderId: "170247498060",
+    appId: "1:170247498060:web:f0da44e9d401d7d983c7e9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
--- js/main.js
+++ js/main.js
@@ -13,3 +13,32 @@ $(".owl-carousel").owlCarousel ( {
         },
     },
 });
+
+
+// contact data
+
+const contactForm = document.querySelector('.contact-form');
+const fullName = document.querySelector(".full-name");
+const emailAddress = document.querySelector(".email-address");
+const subject = document.querySelector(".subject");
+const message = document.querySelector(".message");
+const submit = document.querySelector(".submit-form");
+const modal = document.querySelector(".modal-wrap");
+const close = document.querySelector(".modal-close");
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    db.collection('contact-form').doc().set({
+        fullName: fullName.value,
+        email: emailAddress.value,
+        subject: subject.value,
+        message: message.value,
+    }).then(() => {
+        contactForm.reset();
+        modal.classList.toggle('display-none');
+    })
+});
+
+close.addEventListener('click', ()=> {
+    modal.classList.toggle('display-none');
+})
